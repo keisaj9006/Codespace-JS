bf6e152250a7a586f0ecbaf649eca94f37383f17
keisaj9006 <144917448+keisaj9006@users.noreply.github.com>
2023-11-07 06:07:22 +0000

    Add files via upload

--- JS-basics-Activity2/JS-basics-Activity2.js
+++ JS-basics-Activity2/JS-basics-Activity2.js
@@ -1,3 +1,5 @@
 //Activity 2
-// division 
-console.log('6 / 2 = ', 6 / 2); //mathematical operation with the result
+// division 6 / 2 = 3
+console.log('Input the first number: 6');
+console.log('Input the second number: 2');
+console.log('The division of the first number and the second number is: ', 6 / 2); //mathematical operation with the result
